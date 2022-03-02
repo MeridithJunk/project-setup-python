5fc9a7379dceb1523643632b682e210e3eb7852f
MeridithJunk <Meridith.a.junk@accenture.com>
2022-03-02 10:51:15 -0600

    clean up" git push

--- calculator_pytest.py
+++ calculator_pytest.py
@@ -1,12 +1,14 @@
-import inc_dec    # The code to test
-import unittest   # The test framework
+import calculator  # The code to test
+import unittest  # The test framework
+
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        self.assertEqual(inc_dec.increment(3), 4)
+        self.assertEqual(calculator.increment(3), 4)
 
     def test_decrement(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
